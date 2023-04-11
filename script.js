7263bda35ae8558c30f5a0bfc4b9931d7fd64f43
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-04-11 16:48:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	// for(let ky in sampleObject)
+	// 	{
+	// 		if(ky==key)
+	// 		{
+	// 			return true;
+	// 		}
+	// 	}
+
+	// return false;
+	let key_arr=Object.keys(sampleObject);
+	if(key_arr.includes(key))
+	{
+		return true;
+	}else{
+		return false;
+	}
+	
 }
 
 // Do not change the code below
